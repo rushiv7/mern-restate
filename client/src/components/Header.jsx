795df21d83008a57ee8972aaaab02e88a74170f6
rushiv7 <rushivarkhade@gmail.com>
2023-11-20 11:05:30 +0530

    Show avatar in header and hide profile route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -31,7 +33,15 @@ export default function Header() {
             </li>
           </Link>
           <Link to="/sign-in">
-            <li className="text-slate-700 hover:underline">Sign in</li>
+            {currentUser ? (
+              <img
+                className="rounded-full w-7 h-7 object-cover"
+                src={currentUser.avatar}
+                alt="Profile"
+              />
+            ) : (
+              <li className="text-slate-700 hover:underline">Sign in</li>
+            )}
           </Link>
         </ul>
       </div>
