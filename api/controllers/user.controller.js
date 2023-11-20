29effaec516e3de562fd1236f084b01921b0a0c3
rushiv7 <rushivarkhade@gmail.com>
2023-11-20 16:40:10 +0530

    Add update user api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,36 @@
+import bcrypt from "bcryptjs";
+import { errorCreator } from "../utils/error.js";
+import User from "../models/user.model.js";
+
 export const test = (req, res) => {
   res.json({
     message: "test api res working!",
   });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user._id !== req.params.id)
+    return next(errorCreator(401, "You can only update your own account"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcrypt.hashSync(req.body.password, 10);
+    }
+    const update = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...rest } = update._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
