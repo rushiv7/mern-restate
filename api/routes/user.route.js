e6d625efc6150d6c9b851e3e1bb38de9c7423c47
rushiv7 <rushivarkhade@gmail.com>
2023-11-22 14:46:23 +0530

    Add get user listings API

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   deleteUser,
+  getUserListings,
   test,
   updateUser,
 } from "../controllers/user.controller.js";
@@ -11,5 +12,6 @@ const userRouter = express.Router();
 userRouter.get("/test", test);
 userRouter.post("/update/:id", verifyToken, updateUser);
 userRouter.delete("/delete/:id", verifyToken, deleteUser);
+userRouter.get("/listings/:id", verifyToken, getUserListings);
 
 export default userRouter;
