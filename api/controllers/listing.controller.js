9765f45d909064dfb215296fe5691e0c8c15fe86
rushiv7 <rushivarkhade@gmail.com>
2023-11-23 17:07:37 +0530

    Add update listing API

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -14,13 +14,32 @@ export const createListing = async (req, res, next) => {
 export const deleteListing = async (req, res, next) => {
   try {
     const listing = await Listing.findById(req.params.id);
-    if (!listing) next(errorCreator(404, "Listing not found"));
+    if (!listing) return next(errorCreator(404, "Listing not found"));
 
     if (listing.userRef !== req.user.id)
       return next(errorCreator(401, "You can only delete your own listing"));
 
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json("Listing has been deleted!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) return next(errorCreator(404, "Listing not found"));
+
+    if (listing.userRef !== req.user.id)
+      return next(errorCreator(404, "You can only update your own listing"));
+
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
